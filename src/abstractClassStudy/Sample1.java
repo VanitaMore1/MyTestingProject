76f6eb09cbf00728d3bab60b301c329187f75d66
VanitaMore1 <156526518+VanitaMore1@users.noreply.github.com>
2024-01-14 02:15:06 +0530

    Update Sample1.java

--- src/abstractClassStudy/Sample1.java
+++ src/abstractClassStudy/Sample1.java
@@ -5,7 +5,8 @@ public class Sample1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("hi");
+		System.out.println("hi vanita");
+		
 	}
 
 }
